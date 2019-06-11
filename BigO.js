40ebb44d3ecfff9e129233ba074a301852efaa03
David Nordeen <davidnordeen@gmail.com>
2019-06-11 10:43:00 -0700

    fixed

--- BigO.js
+++ BigO.js
@@ -1,23 +1,23 @@
 'use strict';
 
-1. 
-A. O(1)
-B. O(n)
+// 1. 
+// A. O(1)
+// B. O(n)
 
-2. O(1) The function only takes in a single value, and only has one check
+// 2. O(1) The function only takes in a single value, and only has one check
 
-3. O(n^k) This function requires two levels of looping
+// 3. O(n^k) This function requires two levels of looping
 
-4. O(n) this function iterates over each value in the array and doubles them, so it would take twice as long to complete the process if it had twice as many values
+// 4. O(n) this function iterates over each value in the array and doubles them, so it would take twice as long to complete the process if it had twice as many values
 
-5.O(n) it would take twice as long to complete the process if it had twice as many values
+// 5.O(n) it would take twice as long to complete the process if it had twice as many values
 
-6. O(n^k) for each value in the array, the function loops twice
+// 6. O(n^k) for each value in the array, the function loops twice
 
-7. O(n) one loop, fib sequence
+// 7. O(n) one loop, fib sequence
 
-8.O(log n) max run time is half the amount of values in the array
+// 8.O(log n) max run time is half the amount of values in the array
 
-9. O(1)
+// 9. O(1)
 
-10. O(n) prime number checker
+// 10. O(n) prime number checker
